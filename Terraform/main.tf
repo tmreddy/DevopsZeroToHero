cfefcdbb0c2daabacc29d39a302341e65727d1a0
nlaxmi1260 <yaram.nlaxmi@gmail.com>
2024-02-21 14:47:05 +0530

    DevOpsZerotoHeroFiles

---
+++ Terraform/main.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+# default region is us-east-1
+
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform_Demo"
+  }
+}
